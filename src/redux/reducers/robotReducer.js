c35b3fea87e3ab88bb6333cf78b1b56c8ac199f2
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2021-11-06 19:20:50 +0100

    Added robotReducer

---
+++ src/redux/reducers/robotReducer.js
@@ -0,0 +1,21 @@
+import actionTypes from "../actions/actionTypes";
+
+const robotReducer = (robot = {}, action) => {
+  let newRobot;
+
+  switch (action.type) {
+    case actionTypes.loadRobot:
+      newRobot = { ...action.robot };
+      break;
+    case actionTypes.updateRobot:
+      newRobot = { ...action.robot };
+      break;
+    default:
+      newRobot = { ...robot };
+      break;
+  }
+
+  return newRobot;
+};
+
+export default robotReducer;
