7ae75442732ac0145e6efdbb3cde7c8aa1dd579a
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2021-11-06 18:25:52 +0100

    Added Form component with functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,15 @@
-const Form = () => {
+const Form = ({ addRobot }) => {
+  const onSubmit = (event) => {
+    event.preventDefault();
+    addRobot();
+  };
+
   return (
-    <form className="flex flex-col w-full p-10 px-8 pt-6 mx-auto my-6 mb-4 bg-white border rounded-lg space-y-0 transition duration-500 ease-in-out transform lg:w-1/2">
+    <form
+      autoComplete="off"
+      onSubmit={onSubmit}
+      className="flex flex-col w-full p-10 px-8 pt-6 mx-auto my-6 mb-4 bg-white border rounded-lg space-y-0 transition duration-500 ease-in-out transform lg:w-1/2"
+    >
       <div className="relative pt-4">
         <label htmlFor="name" className="text-base leading-7 text-blueGray-500">
           Name
--- src/components/pages/RobotsMainPage.js
+++ src/components/pages/RobotsMainPage.js
@@ -51,12 +51,13 @@ function RobotsMainPage() {
 
   const deleteRobot = () => {};
   const updateRobot = () => {};
+  const addRobot = () => {};
 
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen py-20 bg-gradient-to-tr from-gray-800 to-blue-400">
         <h1>Wellcome to RobotLand - Oleguer</h1>
-        <Form />
+        <Form addRobot={addRobot} />
         <div className="md:px-4 md:grid md:grid-cols-3 lg:grid-cols-4 gap-5 space-y-4 md:space-y-0">
           {robots.map((robot) => (
             <Robot
