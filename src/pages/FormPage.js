72169ff60ff4e14a95be8f1ef1f08e548bbdd51e
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2021-11-07 13:05:46 +0100

    Refactored pages to navigate and added header

---
+++ src/pages/FormPage.js
@@ -0,0 +1,27 @@
+import Form from "../components/Form";
+import useRobots from "../hooks/useRobots";
+
+function FormPage() {
+  const { addRobot } = useRobots();
+
+  const onClickAdd = (robot) => {
+    const parsedRobot = {
+      name: robot.name,
+      img: robot.img,
+      features: {
+        speed: robot.speed,
+        resistance: robot.resistance,
+        birth: robot.date,
+      },
+    };
+    addRobot(parsedRobot);
+  };
+
+  return (
+    <>
+      <Form addRobot={onClickAdd} />
+    </>
+  );
+}
+
+export default FormPage;
