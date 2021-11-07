78bc6228920eae905e5e32ec15d525cbab356ec6
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2021-11-07 03:05:18 +0100

    Amplified form test to check button disabled or not

--- src/components/Form.test.js
+++ src/components/Form.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Form from "./Form";
 
 describe("Given a Form component", () => {
@@ -20,15 +21,32 @@ describe("Given a Form component", () => {
       expect(button).toBeInTheDocument();
     });
   });
-  describe("When it's rendered with a function add and button clicked", () => {
-    test("Then the function add should have been called", () => {
+  describe("When it's rendered with a function add, EMPTY inputs and button clicked", () => {
+    test("Then the function add should have NOT been called", () => {
       const addFn = jest.fn();
 
       render(<Form addRobot={addFn} />);
       const button = screen.getByRole("button", { name: /add robot/i });
       button.click();
 
-      expect(addFn).toHaveBeenCalled();
+      expect(addFn).not.toHaveBeenCalled();
+    });
+  });
+  describe("When it's rendered with a function add, filled inputs and button clicked", () => {
+    test("Then the function add should have NOT been called", () => {
+      const addFn = jest.fn();
+
+      render(<Form addRobot={addFn} />);
+      const name = screen.getByLabelText(/name/i);
+      const date = screen.getByLabelText(/date/i);
+      userEvent.type(name, "whatever");
+      userEvent.type(date, "whatever");
+
+      const button = screen.getByRole("button", { name: /add robot/i });
+
+      button.click();
+
+      expect(addFn).not.toHaveBeenCalled();
     });
   });
 });
