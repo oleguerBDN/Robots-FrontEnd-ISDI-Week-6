fab1f65085d7a15051dda6e57bfa6c3190717e5d
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2021-11-07 01:34:52 +0100

    Added deletRobotThunk

--- src/redux/thunks/robotsThunks.js
+++ src/redux/thunks/robotsThunks.js
@@ -1,7 +1,18 @@
-import { loadRobotsAction } from "../actions/actionCreators";
+import { loadRobotsAction, deleteRobotAction } from "../actions/actionCreators";
 
 export const loadRobotsThunk = async (dispatch) => {
   const response = await fetch(process.env.REACT_APP_URL_API);
   const robots = await response.json();
   dispatch(loadRobotsAction(robots));
+};
+
+export const deleteRobotThunk = (robotId) => async (dispatch) => {
+  const tokenString = "?token=" + process.env.TOKEN;
+  const response = await fetch(
+    process.env.REACT_APP_URL_API + robotId + tokenString,
+    { method: "DELETE" }
+  );
+  if (response.ok) {
+    dispatch(deleteRobotAction(robotId));
+  }
 };
